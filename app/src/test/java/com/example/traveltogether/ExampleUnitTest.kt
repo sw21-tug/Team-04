b275116e9228269c709104383d60213e5cda92c5
Laila ElShafie <laila.elshafie@student.tugraz.at>
2021-05-04 23:03:51 +0200

    TT-019 UI of post [EL,AbdelhalimM]

--- app/src/test/java/com/example/traveltogether/ExampleUnitTest.kt
+++ app/src/test/java/com/example/traveltogether/ExampleUnitTest.kt
@@ -4,10 +4,6 @@ import android.content.ContentValues
 import android.util.Log
 import com.google.firebase.database.*
 import org.junit.Test
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
 import com.firebase.ui.auth.AuthUI
 
 
