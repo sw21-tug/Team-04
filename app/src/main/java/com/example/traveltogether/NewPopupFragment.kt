f94a8f46a2534eab41ba012873b08da4cf57c9cd
Manar Abdelhalim <manar.abdelhalim@student.tugraz.at>
2021-05-05 00:35:54 +0200

    TT-019 ADD POSt display [EL, AbdelhalimM]

--- app/src/main/java/com/example/traveltogether/NewPopupFragment.kt
+++ app/src/main/java/com/example/traveltogether/NewPopupFragment.kt
@@ -34,7 +34,7 @@ class new_popup_fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_popup_fragment, container, false)
+        return inflater.inflate(R.layout.activity_edit_post, container, false)
     }
 
     companion object {
