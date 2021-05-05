234524a6cf09a787c197f5fa36caffdf648560fc
Frederik <frederik.wagner@student.tugraz.at>
2021-05-05 20:38:18 +0200

    TT-013 delete changes [BA,WF]

--- app/src/main/java/com/example/traveltogether/UserPost.kt
+++ app/src/main/java/com/example/traveltogether/UserPost.kt
@@ -4,14 +4,16 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import java.util.*
 
-class UserPost (val UID: String, val PID: Int, var Title: String, var Destination: String, var StartDate: Date?, var EndDate: Date?,
-                var NumOfPeople: Int, var Description: String, var Comments: List<String>?) {
+class UserPost (val UID: String, var PID: String?, var Title: String, var Destination: String, var StartDate: Long, var EndDate: Long,
+                var NumOfPeople: Int?, var Description: String, var Comments: List<String>?){
 
     fun delete(): Boolean{
         if (UID != FirebaseAuth.getInstance().currentUser?.uid.toString())
             return false
+        if(PID == null)
+            return false
         val firebasereal = FirebaseDatabase.getInstance()
-        val firebaseref = firebasereal.reference.child("users").child(FirebaseAuth.getInstance().currentUser?.uid.toString()).child("posts").child(PID.toString()).ref
+        val firebaseref = firebasereal.reference.child("posts").child(PID!!).ref
         firebaseref.removeValue()
         return true
     }
