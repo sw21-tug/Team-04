d905f352a838f0bc55c1fa63ab8b3c98e004c06b
Manar Abdelhalim <manar.abdelhalim@student.tugraz.at>
2021-05-05 22:21:34 +0200

    TT-010 Tests for Add Post [AbdelhalimM, SM]]

--- app/src/androidTest/java/com/example/traveltogether/EditPostTest.kt
+++ app/src/androidTest/java/com/example/traveltogether/EditPostTest.kt
@@ -1,27 +1,74 @@
 package com.example.traveltogether
 
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Before
+import org.junit.Rule
 
 import org.junit.Test
 import org.junit.runner.RunWith
-
 @RunWith(AndroidJUnit4::class)
 class EditPostTest {
 
-    @Test //how to test UI elements
-    fun checkpopupfragment() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    private lateinit var loginUser: LoginUser
+
+    @Before
+    fun setup() {
+        loginUser = LoginUser("markus123@gmail.com", "Markus", "markus123", "Hallo")
+        loginUser.signIn()
         onView(withId(R.id.new_popup_fragment)).perform(click())
+    }
+
+    @Test
+    fun checkPopupFragment() {
         onView(withId(R.id.title_edit_post)).perform(typeText("bus trip"))
         onView(withId(R.id.destination_text_post)).perform(typeText("Ohio"))
-        onView(withId(R.id.starting_date_post_text)).perform(typeText("03/06/2020"))
+        onView(withHint(R.string.starting_date)).check(matches(isDisplayed()))
+        onView(withHint(R.string.ending_date)).check(matches(isDisplayed()))
         onView(withId(R.id.number_people_post_text)).perform(typeText("4"))
-        onView(withId(R.id.end_date_post_text)).perform(typeText("03/06/2020"))
         onView(withId(R.id.description_post_text)).perform(typeText("Trip with bus"))
     }
+
+    private fun getRandomString(size: Int ) : String {
+        val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        return (1..size)
+                .map { _ -> kotlin.random.Random.nextInt(0, charPool.size) }
+                .map(charPool::get)
+                .joinToString("")
+    }
+
+    @Test
+    fun addPost() {
+        val testTitle = getRandomString(10)
+        val testLocation = getRandomString(5)
+        val testDescription = getRandomString(15)
+        val numberOfPeople = kotlin.random.Random.nextInt(0, 10).toString()
+
+        onView(withId(R.id.title_edit_post)).perform(typeText(testTitle))
+        onView(withId(R.id.destination_text_post)).perform(typeText(testLocation))
+        onView(withHint(R.string.starting_date)).perform(click())
+        onView(withText("OK")).perform(click())
+        onView(withId(R.id.number_people_post_text)).perform(typeText(numberOfPeople))
+        pressBack()
+        onView(withId(R.id.description_post_text)).perform(typeText(testDescription))
+        pressBack()
+        onView(withId(R.id.button_save_new_post)).perform(click())
+
+        onView(withHint(R.string.title)).check(matches(isDisplayed()))
+        onView(withHint(R.string.destination)).check(matches(isDisplayed()))
+        onView(withHint(R.string.number_people)).check(matches(isDisplayed()))
+        onView(withHint(R.string.starting_date)).check(matches(isDisplayed()))
+        onView(withHint(R.string.ending_date)).check(matches(isDisplayed()))
+    }
+
 }
