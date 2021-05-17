b109791d8fa1e406e1be4bbccccbc82da3417564
StellaLukasser <stella.lukasser@student.tugraz.at>
2021-05-17 17:56:59 +0200

    TT-002 adapted layout of edit and add post to new design [AM, LS]

--- app/src/main/java/com/example/traveltogether/PostsAdapter.kt
+++ app/src/main/java/com/example/traveltogether/PostsAdapter.kt
@@ -10,7 +10,6 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_post.view.*
 import kotlinx.android.synthetic.main.item_post.view.*
 import kotlinx.android.synthetic.main.item_post.view.destination
 import java.text.SimpleDateFormat
