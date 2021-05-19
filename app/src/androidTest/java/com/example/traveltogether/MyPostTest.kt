852b0a3a7ccaed304c657e05ddad513b92879b84
Schleifinho <30636595+Schleifinho@users.noreply.github.com>
2021-05-19 12:07:15 +0200

    TT-017 Added MyPost Tests [BA, SM]

--- app/src/androidTest/java/com/example/traveltogether/MyPostTest.kt
+++ app/src/androidTest/java/com/example/traveltogether/MyPostTest.kt
@@ -2,7 +2,6 @@ package com.example.traveltogether
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -10,37 +9,44 @@ import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.text.SimpleDateFormat
+import java.util.*
 
 @RunWith(androidx.test.ext.junit.runners.AndroidJUnit4::class)
 class MyPostTest {
     private lateinit var loginUser: LoginUser
     private lateinit var firebaseDb: FirebaseDatabase
     private lateinit var firebaseRef: DatabaseReference
-    private val string = "test comment"
-    private val postTitle = "Urlaub"
+    private val postTitle = "Weit weg"
+    private val postDestination = "Weg"
+    private val postDescription = "...."
+    private val postStartEndDate = 1621209800000L
+    private val postNPeople = 3L
     private lateinit var pid : String
     private lateinit var userPost : UserPost
+    private val myFormat = "dd/MM/yyyy"
 
     @get:Rule
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Before
     fun checkLogin () {
-        loginUser = LoginUser("test@gmail.com", "Name", "12345678", "")
+        loginUser = LoginUser("markus123@gmail.com", "Markus", "markus123", "")
         loginUser.signIn()
         firebaseDb = FirebaseDatabase.getInstance()
         firebaseRef = firebaseDb.reference
         val list : MutableList<Comment> = mutableListOf()
         firebaseRef.child("posts").push().
         setValue(UserPost(
-            FirebaseAuth.getInstance().currentUser?.uid.toString(), "1", System.currentTimeMillis(),
-                postTitle, "Malle", 1, 1,
-            3, "hallo", list))
+            FirebaseAuth.getInstance().currentUser?.uid.toString(), "2", System.currentTimeMillis(),
+                postTitle, postDestination, postStartEndDate, postStartEndDate,
+                postNPeople, postDescription, list))
         val data = Tasks.await(firebaseRef.child("posts").get())
 
 
@@ -69,13 +75,59 @@ class MyPostTest {
 
     @After
     fun cleanup () {
+        var activityRule = ActivityScenarioRule(MainActivity::class.java)
+        onView(withId(R.id.saved_post_fragment)).perform(click())
         userPost.delete()
     }
 
     @Test
-    fun checkDisplay() {
+    fun checkDisplayContainer() {
         onView(withId(R.id.saved_post_fragment)).perform(click())
         onView(withId(R.id.recycler_view_my_post)).check(matches(isDisplayed()))
     }
 
+    @Test
+    fun checkIfPostIsDisplayed() {
+        onView(withId(R.id.saved_post_fragment)).perform(click())
+        onView(withText(postTitle)).check(matches(isDisplayed()))
+        onView(withText(postDestination)).check(matches(isDisplayed()))
+        onView(withText(postDescription)).check(matches(isDisplayed()))
+        onView(withText("Group size: $postNPeople")).check(matches(isDisplayed()))
+
+        val date = getDate(postStartEndDate)
+        onView(withText("From $date")).check(matches(isDisplayed()))
+        onView(withText("To $date")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun checkIfPostEditButtonClickable() {
+        onView(withId(R.id.saved_post_fragment)).perform(click())
+        onView(allOf(withText("Edit Post"), hasSibling(withText(postTitle))))
+                .perform(click())
+
+    }
+
+    @Test
+    fun checkIfPostCommentButtonClickable() {
+        onView(withId(R.id.saved_post_fragment)).perform(click())
+        onView(allOf(withText("Comments"), hasSibling(withText(postTitle))))
+                .perform(click())
+    }
+
+    @Test
+    fun checkIfPostJoinButtonClickable() {
+        onView(withId(R.id.saved_post_fragment)).perform(click())
+        onView(allOf(withText("Join Group Chat"), hasSibling(withText(postTitle))))
+                .perform(click())
+    }
+
+    private fun getDate(l: Long): String? {
+        return try {
+            val sdf = SimpleDateFormat(myFormat)
+            val netDate = Date(l)
+            sdf.format(netDate)
+        } catch (e: Exception) {
+            e.toString()
+        }
+    }
 }
