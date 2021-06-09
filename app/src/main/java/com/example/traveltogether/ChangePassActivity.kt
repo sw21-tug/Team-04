6a016b09ac9a58ba93b994747e65106f10ddae69
Manar Allam <manar.allam@student.tugraz.at>
2021-06-09 21:45:56 +0200

    TT-038 bugfix disable keyboard [AbdelhalimM, AllamM]

--- app/src/main/java/com/example/traveltogether/ChangePassActivity.kt
+++ app/src/main/java/com/example/traveltogether/ChangePassActivity.kt
@@ -1,11 +1,14 @@
 package com.example.traveltogether
 
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -113,5 +116,13 @@ class ChangePassActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         startActivity(intentFor<MainActivity>().newTask().clearTask())
+    }
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
--- app/src/main/java/com/example/traveltogether/MainActivity.kt
+++ app/src/main/java/com/example/traveltogether/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.traveltogether
 
+import android.content.Context
 import com.firebase.ui.auth.AuthUI
 import org.jetbrains.anko.intentFor
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -67,5 +70,20 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.buttom_menu_fragment)
         return navController.navigateUp(appBarConfiguration)
                 ||  super.onSupportNavigateUp()
+    }
+    private fun closeKeyBoard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
